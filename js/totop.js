9b390c97ab45d4070a5a54dfce7b38c7bc3f6546
sheng09 <wangsheng.cas@gmail.com>
2016-06-14 15:40:06 +0800

    g

--- js/totop.js
+++ js/totop.js
@@ -1,21 +1,18 @@
-(function($) { 
+(function($) {
 	// When to show the scroll link
-	// higher number = scroll link appears further down the page   
+	// higher number = scroll link appears further down the page
 	var upperLimit = 1000;
-	
 	// Our scroll link element
 	var scrollElem = $('#totop');
-   
 	// Scroll to top speed
 	var scrollSpeed = 500;
-   
-	// Show and hide the scroll to top link based on scroll position   
+	// Show and hide the scroll to top link based on scroll position
 	scrollElem.hide();
-	$(window).scroll(function () {            
-		var scrollTop = $(document).scrollTop();       
+	$(window).scroll(function () {
+		var scrollTop = $(document).scrollTop();
 		if ( scrollTop > upperLimit ) {
-			$(scrollElem).stop().fadeTo(300, 1); // fade back in           
-		}else{       
+			$(scrollElem).stop().fadeTo(300, 1); // fade back in
+		}else{
 			$(scrollElem).stop().fadeTo(300, 0); // fade out
 		}
 	});
