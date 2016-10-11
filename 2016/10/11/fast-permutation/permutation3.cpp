66ca155cf5d6535c5ff0502bdf65c630b6baa2ed
sheng09 <wangsheng.cas@gmail.com>
2016-10-12 02:19:28 +0800

    g

---
+++ 2016/10/11/fast-permutation/permutation3.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <ctime>
+#include <string>
+static int n;
+static int end;
+static int count = 0;
+static int  *sol  = new int  [n] {};
+static int avai = 0;
+void print() {
+    for(int i =0; i < n; ++i) std::cout << sol[i];
+    std::cout << ";" << count << "\n";
+}
+void permutation( int pos ) {
+    for(int i = 0; i < n; ++i) {
+        if( ( avai & (1<<i) ) == 0 ) {
+            //sol[pos] = i;
+            if (pos == end) {
+                count++;
+                //print();
+            }
+            else {
+                avai ^= (1<<i);
+                permutation(pos+1);
+                avai ^= (1<<i);
+            }
+        }
+    }
+}
+
+int main(int argc, char const *argv[]) {
+    n = std::stoi(argv[1]);
+    if( argc == 3)
+        end = std::stoi(argv[2]) - 1;
+    else
+        end = n - 1;
+    std::clock_t tic = std::clock();
+    permutation(0);
+    std::clock_t toc = std::clock();
+    std::cout << "Solution: " << count << " " << float(toc - tic) / CLOCKS_PER_SEC << "s\n";
+    return 0;
+}
---
+++ 2016/10/11/fast-permutation/permutation5.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <ctime>
+#include <string>
+static int n;
+static int end;
+static int count = 0;
+static int  *sol  = new int  [n] {};
+void print() {
+    for(int i =0; i < n; ++i) std::cout << sol[i];
+    std::cout << ";" << count << "\n";
+}
+void permutation( int pos, int avai ) {
+    int empty =  ((1<<n)-1) & ~avai;
+    while( empty != 0 ) {
+        int p = empty & (-empty);
+        empty ^= p;
+        if (pos == end) {
+            count++;
+        }
+        else  {
+            permutation(pos+1, avai^p);
+        }
+    }
+}
+
+int main(int argc, char const *argv[]) {
+    n = std::stoi(argv[1]);
+    if( argc == 3)
+        end = std::stoi(argv[2]) - 1;
+    else
+        end = n - 1;
+    std::clock_t tic = std::clock();
+    permutation(0, 0);
+    std::clock_t toc = std::clock();
+    std::cout << "Solution: " << count << " " << float(toc - tic) / CLOCKS_PER_SEC << "s\n";
+    return 0;
+}
